880262830f4484b12d9b25d911077a9e898cb526
Patrick Mariano <99706405+patrickmariano04@users.noreply.github.com>
2023-02-07 21:09:21 +0800

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/vendor/autoload.php";
- $client = new MongoDB\Client("mongodb+srv://pat:pat@cluster0.94f6xud.mongodb.net/test");
-$db = $client->test;
-$db = $client->sample_restaurants;
-$collection = $db->restaurants;
-
-?>
